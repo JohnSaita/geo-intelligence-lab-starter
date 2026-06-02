8af7ff70ce3558b84705fd1abcb923eeb598e26b
JohnSaita <jsaita47@gmail.com>
2026-06-02 19:34:12 +0300

    Update ploting of both point, line and polygon features

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -31,16 +31,58 @@ export default function MapContainer() {
         mapStyle="mapbox://styles/mapbox/streets-v12"
         style={{ width: "100%", height: "100%" }}
       >
-        <Source id="cities" type="geojson" data={geojsonData}>
+        <Source id="geojson" type="geojson" data={geojsonData}>
+          {/* Points */}
           <Layer
-            id="cities-layer"
+            id="points-layer"
             type="circle"
+            filter={[
+              "any",
+              ["==", ["geometry-type"], "Point"],
+              ["==", ["geometry-type"], "MultiPoint"],
+            ]}
             layout={{
               visibility: layerVisible ? "visible" : "none",
             }}
             paint={{
               "circle-radius": 6,
               "circle-color": "#007cbf",
+            }}
+          />
+
+          {/* Lines */}
+          <Layer
+            id="lines-layer"
+            type="line"
+            filter={[
+              "any",
+              ["==", ["geometry-type"], "LineString"],
+              ["==", ["geometry-type"], "MultiLineString"],
+            ]}
+            layout={{
+              visibility: layerVisible ? "visible" : "none",
+            }}
+            paint={{
+              "line-color": "#ff0000",
+              "line-width": 3,
+            }}
+          />
+
+          {/* Polygon boundaries */}
+          <Layer
+            id="polygon-boundaries"
+            type="line"
+            filter={[
+              "any",
+              ["==", ["geometry-type"], "Polygon"],
+              ["==", ["geometry-type"], "MultiPolygon"],
+            ]}
+            layout={{
+              visibility: layerVisible ? "visible" : "none",
+            }}
+            paint={{
+              "line-color": "#374151",
+              "line-width": 1.5,
             }}
           />
         </Source>
